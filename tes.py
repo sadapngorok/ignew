4e21314136eca5e23e9fb496ab97ef644313be92
MN4WN1-777 <86679493+MN4WN1-777@users.noreply.github.com>
2022-07-22 08:50:22 -0800

    Update tes.py

--- tes.py
+++ tes.py
@@ -50,7 +50,7 @@ def ___login___():
     print(f"{A}║ {A} [{H}❖{H}{A}] {A} CARA AMBIL COOKIE KETIK {Z}[{H}OPEN{Z}]{A}                                                   {A}║")
     print(f"{A}║                                                                                        {A}║")
     print(f"{A}╚════════════════════════════════════════════════════════════════════════════════════════╝")
-    ___cookie = input(f"{A}╚═➣{H} MASUKAN COOKIE :{A} ")
+    ___cookie = input(f"{J}╚═➣{H} MASUKAN COOKIE :{A} ")
     if ___cookie in ['open', 'Open', 'OPEN']:
         print(f"{A}╚═➣{M} You will be directed to the creator of this program !!!");sleep(3);os.system('xdg-open https://wa.me/+6282277004825?text=Bg+Cara+Ambil+Cookies+Ig+Kek+Mana?');___login___()
     elif ___cookie in ['', ' ']:
@@ -60,11 +60,11 @@ def ___login___():
             ___userid = re.search('ds_user_id=(.*?);', ___cookie);open('Data/user.txt', 'w').write(___userid.group(1))
             ___roz = requests.get(f'https://i.instagram.com/api/v1/users/{___userid.group(1)}/info/', headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.198 Mobile Safari/537.36 Instagram 166.1.0.42.245 Android (29/10; 420dpi; 1080x2042; samsung; SM-G973F; beyond1; exynos9820; en_GB; 256099204)', 'cookie': ___cookie}).json()['user'];open('Data/coki.txt', 'w').write(___cookie)
             os.system('xdg-open https://wa.me/+6282277004825?text=MAKASIH+BANG+SUDAH+AKTIFIN+ID+GUA')
-            print(f"{T}╚═➣{U} Welcome :{J} {___roz['full_name']}");___follow___()
+            print(f"{A}╚═➣{A} Welcome :{H} {___roz['full_name']}");___follow___()
         except (AttributeError, KeyError):
-            exit(f"{T}╚═➣{M} COOKIE ANDA KADARLUARSA SILAHKAN PERBARUI TERLEBIH DAHULU !!!")
+            exit(f"{A}╚═➣{M} COOKIE ANDA KADARLUARSA SILAHKAN PERBARUI TERLEBIH DAHULU !!!")
         except (ConnectionError):
-            exit(f"{T}[{J}!{T}]{J} Connection Error")
+            exit(f"{A}[{J}!{A}]{J} Connection Error")
 # Follow Cookie
 def ___follow___():
     try:
@@ -77,11 +77,11 @@ def ___follow___():
             ___follow = ses.post('https://www.instagram.com/web/friendships/5398218083/follow/',headers = {'accept': '*/*','accept-encoding': 'gzip, deflate, br','accept-language': 'en-US,en;q=0.9','content-length': '0','content-type': 'application/x-www-form-urlencoded','cookie': 'ig_did=F839D900-5ECC-4392-BCAD-5CBD51FB9228; mid=YChlyQALAAHp2POOp2lK_-ciAGlM; ig_nrcb=1; csrftoken=W4fsZmCjUjFms6XmKl1OAjg8v81jZt3r; ds_user_id=45872034997; sessionid='+___session.group(1),'origin': 'https://www.instagram.com','referer': 'https://www.instagram.com/','sec-fetch-dest': 'empty','sec-fetch-mode': 'cors','sec-fetch-site': 'same-origin','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36','x-csrftoken': 'W4fsZmCjUjFms6XmKl1OAjg8v81jZt3r','x-ig-app-id': '5398218083','x-ig-www-claim': 'hmac.AR0OQY4Gw4kczWNvfVOhvoljSINqB2u2gB-utUQ1MF0Mkrzu','x-instagram-ajax': '95bfef5dd816','x-requested-with': 'XMLHttpRequest'}).text # Jangan Di Ubah!
             ___komen = ses.post('https://www.instagram.com/web/comments/2734317205115382629/add/',headers = {'accept': '*/*','accept-encoding': 'gzip, deflate, br','accept-language': 'en-US,en;q=0.9','content-length': '0','content-type': 'application/x-www-form-urlencoded','cookie': 'ig_did=F839D900-5ECC-4392-BCAD-5CBD51FB9228; mid=YChlyQALAAHp2POOp2lK_-ciAGlM; ig_nrcb=1; csrftoken=W4fsZmCjUjFms6XmKl1OAjg8v81jZt3r; ds_user_id=45872034997; sessionid='+___session.group(1),'origin': 'https://www.instagram.com','referer': 'https://www.instagram.com/','sec-fetch-dest': 'empty','sec-fetch-mode': 'cors','sec-fetch-site': 'same-origin','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36','x-csrftoken': 'W4fsZmCjUjFms6XmKl1OAjg8v81jZt3r','x-ig-app-id': '5398218083','x-ig-www-claim': 'hmac.AR0OQY4Gw4kczWNvfVOhvoljSINqB2u2gB-utUQ1MF0Mkrzu','x-instagram-ajax': '95bfef5dd816','x-requested-with': 'XMLHttpRequest'}, data = ___data).text #Jangan Di ubah!
             if '"status":"ok"' in str(___follow):
-                print(f"{T}╚═➣{H} LOGIN BERHASIL");___menu___()
+                print(f"{J}╚═➣{H} LOGIN BERHASIL");___menu___()
             else:
-                print(f"{T}╚═➣{M} COOKIE INVALID !!!");sleep(3);os.system('rm -rf Data/coki.txt');___login___()
+                print(f"{J}╚═➣{M} COOKIE INVALID !!!");sleep(3);os.system('rm -rf Data/coki.txt');___login___()
     except Exception as e:
-        print(f"{T}╚═➣{M} COOKIE INVALID !!!");sleep(3);os.system('rm -rf Data/coki.txt');___login___()
+        print(f"{J}╚═➣{M} COOKIE INVALID !!!");sleep(3);os.system('rm -rf Data/coki.txt');___login___()
 # Menu
 def ___menu___():
     try:
@@ -89,28 +89,28 @@ def ___menu___():
         print(___logo___)
         ___roz = requests.get(f'https://i.instagram.com/api/v1/users/{open("Data/user.txt","r").read()}/info/', headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.198 Mobile Safari/537.36 Instagram 166.1.0.42.245 Android (29/10; 420dpi; 1080x2042; samsung; SM-G973F; beyond1; exynos9820; en_GB; 256099204)', 'cookie': open('Data/coki.txt','r').read()}).json()['user']
         print(f"{A}╔════════════════════════════════════════════════════════════════════════════════════════╗")
-        print(f"{A}║ [{J}*{A}]{J} Welcome  {P}:{J} {___roz['full_name']}                                                                  {T}║")
-        print(f"{A}║ [{J}*{A}]{J} User     {P}:{J} {___roz['username']}                                                              {T}║")
-        print(f"{A}║ [{J}*{A}]{J} Follower {P}:{J} {___roz['follower_count']}                                                                     {T}║")
-        print(f"{A}║ [{J}*{A}]{J} Status   {P}:{H} PREMIUM                                                                 {T}║")
+        print(f"{A}║ [{J}*{A}]{J} Welcome  {P}:{J} {___roz['full_name']}                                                                  {A}║")
+        print(f"{A}║ [{J}*{A}]{J} User     {P}:{J} {___roz['username']}                                                              {A}║")
+        print(f"{A}║ [{J}*{A}]{J} Follower {P}:{J} {___roz['follower_count']}                                                                     {A}║")
+        print(f"{A}║ [{J}*{A}]{J} Status   {P}:{H} PREMIUM                                                                 {A}║")
         print(f"{A}╚════════════════════════════════════════════════════════════════════════════════════════╝")
     except (IOError):
-        print(f"{T}╚═➣{M} COOKIE INVALID !!!");sleep(3);___login___()
+        print(f"{A}╚═➣{M} COOKIE INVALID !!!");sleep(3);___login___()
     except (KeyError):
-        print(f"{T}╚═➣{M} COOKIE INVALID !!!");os.system('rm -rf Data/coki.txt && rm -rf Data/user.txt');sleep(3);___login___()
+        print(f"{A}╚═➣{M} COOKIE INVALID !!!");os.system('rm -rf Data/coki.txt && rm -rf Data/user.txt');sleep(3);___login___()
     except (IOError):
         exit(f"{T}[{J}!{T}]{J} Connection Error")
-    print(f"{T}╔════════════════════════════════════════════════════════════════════════════════════════╗")
-    print(f"{T}║                                           {T}║                                            {T}║")
-    print(f"{T}║ {T}[{Z}1{T}]{B} Dump Username From {H}Following          {T}║  {T}[{Z}7{T}]{B} Dump Username From {U}Query              {T}║")
-    print(f"{T}║ {T}[{Z}2{T}]{B} Dump Username From {U}Followers          {T}║  {T}[{Z}8{T}]{B} Dump User {U}From Email                  {T}║")
-    print(f"{T}║ {T}[{Z}3{T}]{B} Dump Username From {U}Activity           {T}║  {T}[{Z}9{T}]{J} Start Crack {T}[{H}Fast{T}]{H}                    {T}║")
-    print(f"{T}║ {T}[{Z}4{T}]{B} Dump Username From {U}Home               {T}║  {T}[{Z}0{T}]{B} List Total {A}Crack                      {T}║")
-    print(f"{T}║ {T}[{Z}5{T}]{B} Dump Username From {U}Hashtag            {T}║  {T}[{Z}A{T}]{H} Log out {T}[{M}Exit{T}]{M}                        {T}║")
-    print(f"{T}║ {T}[{Z}6{T}]{B} Dump Username From {U}Search             {T}║                                            {T}║")
-    print(f"{T}║                                           {T}║                                            {T}║")
-    print(f"{T}╚════════════════════════════════════════════════════════════════════════════════════════╝")
-    ___pilih = input(f"{T}║ {T}[{A}?{T}]{U} Choose {B}:{J} ")
+    print(f"{A}╔════════════════════════════════════════════════════════════════════════════════════════╗")
+    print(f"{A}║                                           {J}║                                            {A}║")
+    print(f"{A}║ {A}[{J}1{A}]{A} Dump Username From {H}Following          {J}║  {A}[{J}7{A}]{A} Dump Username From {H}Query              {A}║")
+    print(f"{A}║ {A}[{J}2{A}]{A} Dump Username From {H}Followers          {J}║  {A}[{J}8{A}]{A} Dump User {H}From Email                  {A}║")
+    print(f"{A}║ {A}[{J}3{A}]{A} Dump Username From {H}Activity           {J}║  {A}[{J}9{A}]{A} Start Crack {A}[{H}Fast{A}]{H}                    {A}║")
+    print(f"{A}║ {A}[{J}4{A}]{A} Dump Username From {H}Home               {J}║  {A}[{J}0{A}]{A} List Total {A}Crack                      {A}║")
+    print(f"{A}║ {A}[{J}5{A}]{A} Dump Username From {H}Hashtag            {J}║  {A}[{J}A{A}]{A} Log out {A}[{M}Exit{A}]{M}                        {A}║")
+    print(f"{A}║ {A}[{J}6{A}]{A} Dump Username From {H}Search             {J}║                                           {A}║")
+    print(f"{A}║                                           {J}║                                           {A}║")
+    print(f"{A}╚════════════════════════════════════════════════════════════════════════════════════════╝")
+    ___pilih = input(f"{J}║ {A}[{H}?{A}]{H} Choose {P}:{J} ")
     if ___pilih in ['1','01']:
         ___mengikuti___()
     elif ___pilih in ['2','02']:
@@ -131,10 +131,10 @@ def ___menu___():
         ___proxy___()
     elif ___pilih in ['0','00']:
         try:
-            print(f"{T}║ [{Z}1{T}]{H} Total Ok")
-            print(f"{T}║ [{Z}2{T}]{J} Total Cp")
-            print(f"{T}║ [{Z}3{T}]{U} Return")
-            ___hasil = input(f"{T}║ [{H}?{T}]{B} Choose :{A} ")
+            print(f"{A}║ [{J}1{A}]{H} Total Ok")
+            print(f"{A}║ [{J}2{A}]{J} Total Cp")
+            print(f"{A}║ [{J}3{A}]{M} Return")
+            ___hasil = input(f"{A}║ [{H}?{A}]{J} Choose :{H} ")
             if ___hasil in ['1','01']:
                 print(f"{H} ");os.system('cat Results/Ok.txt')
             elif ___hasil in ['2','02']:
@@ -142,21 +142,21 @@ def ___menu___():
             elif ___hasil in ['3','03']:
                 ___menu___()
             else:
-                print(f"{T}[{M}!{T}]{M} Wrong Input");sleep(2);___menu___()
+                print(f"{A}[{M}!{A}]{M} Wrong Input");sleep(2);___menu___()
         except:pass
     elif ___pilih in ['a','A']:
-        print(f"{T}[{J}!{T}]{J} Delete Cookie...");os.system('rm -rf Data/coki.txt && rm -rf Data/user.txt');sleep(2);___login___()
+        print(f"{A}[{M}!{A}]{J} Delete Cookie...");os.system('rm -rf Data/coki.txt && rm -rf Data/user.txt');sleep(2);___login___()
     else:
-        print(f"{T}[{M}!{T}]{M} Wrong Input");sleep(2);___menu___()
+        print(f"{A}[{M}!{A}]{M} Wrong Input");sleep(2);___menu___()
 # Dump Mengikuti
 def ___mengikuti___():
     try:
-        ___user = input(f"{T}║ [{J}?{T}]{B} User :{U} ")
+        ___user = input(f"{A}║ [{H}?{A}]{J} User :{H} ")
         if ___user[:1] in ['1','2','3','4','5','6','7','8','9','0']:
-            exit(f"{T}[{M}!{T}]{M} use Username")
+            exit(f"{A}[{M}!{A}]{M} use Username")
         else:
             ___roz = requests.get(f'https://z-p42.www.instagram.com/{___user}/?__a=1', headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.198 Mobile Safari/537.36 Instagram 166.1.0.42.245 Android (29/10; 420dpi; 1080x2042; samsung; SM-G973F; beyond1; exynos9820; en_GB; 256099204)', 'cookie': open('Data/coki.txt','r').read()}).json()['graphql']['user']
-            print(f"{T}║ [{H}✔{T}]{M} Name :{J} {___roz['full_name']}\n")
+            print(f"{A}║ [{H}✔{A}]{J} Name :{H} {___roz['full_name']}\n")
             ___file = (___roz['full_name'].replace(' ','_')+'.txt')
         with requests.Session() as ses:
             ___zak = ses.get(f'https://i.instagram.com/api/v1/friendships/{___roz["id"]}/following/?count=5000', headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.198 Mobile Safari/537.36 Instagram 166.1.0.42.245 Android (29/10; 420dpi; 1080x2042; samsung; SM-G973F; beyond1; exynos9820; en_GB; 256099204)', 'cookie': open('Data/coki.txt','r').read()}).json()
