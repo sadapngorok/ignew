d67ecd98ccb51811998f6fb26ec633096433bbac
MN4WN1-777 <86679493+MN4WN1-777@users.noreply.github.com>
2022-08-12 01:51:09 -0800

    Update jk.py

--- jk.py
+++ jk.py
@@ -89,7 +89,7 @@ def ___menu___():
         print(___logo___)
         ___roz = requests.get(f'https://i.instagram.com/api/v1/users/{open("Data/user.txt","r").read()}/info/', headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.198 Mobile Safari/537.36 Instagram 166.1.0.42.245 Android (29/10; 420dpi; 1080x2042; samsung; SM-G973F; beyond1; exynos9820; en_GB; 256099204)', 'cookie': open('Data/coki.txt','r').read()}).json()['user']
         print(f"{A}╔════════════════════════════════════════════════════════════════════════════════════════╗")
-        print(f"{A}║ [{J}*{A}]{J} Welcome  {P}:{J} {___roz['full_name']}                                                  {A}║")
+        print(f"{A}║ [{J}*{A}]{J} Welcome  {P}:{J} {___roz['full_name']}                                                 {A}║")
         print(f"{A}║ [{J}*{A}]{J} User     {P}:{J} {___roz['username']}                                                        {A}║")
         print(f"{A}║ [{J}*{A}]{J} Follower {P}:{J} {___roz['follower_count']}                                                                      {A}║")
         print(f"{A}║ [{J}*{A}]{J} Status   {P}:{H} PREMIUM                                                                 {A}║")
